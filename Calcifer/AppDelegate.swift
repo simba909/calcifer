0b8e3beece021abf5be31e19e85862a84d6b181e
Simon Jarbrant <simonjarbrant@gmail.com>
2018-01-26 10:37:24 +0100

    Initial Commit

---
+++ Calcifer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Calcifer
+//
+//  Created by Simon Jarbrant on 2018-01-26.
+//  Copyright © 2018 Simon Jarbrant. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
