2fe5d5fcfa62214370e49b5283efa1e19ca11a3d
Simon Jarbrant <simonjarbrant@gmail.com>
2018-01-26 15:44:20 +0100

    Add ddcctl to submodules

---
+++ Calcifer/Calcifer-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Calcifer-Bridging-Header.h
+//  Calcifer
+//
+//  Created by Simon Jarbrant on 2018-01-26.
+//  Copyright © 2018 Simon Jarbrant. All rights reserved.
+//
+
+#ifndef Calcifer_Bridging_Header_h
+#define Calcifer_Bridging_Header_h
+
+// These two are needed for DDC.h to compile
+#import <Foundation/Foundation.h>
+#import <AppKit/NSScreen.h>
+
+#import "DDC.h"
+
+#endif /* Calcifer_Bridging_Header_h */
